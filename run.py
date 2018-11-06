ce9db3166e7c12fd12a991815c4d833fe328f968
Heather Olcot <hfolcot@gmail.com>
2018-11-06 13:39:16 +0000

    created basic layout of add_recipe.html and set up app route to the page

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+    
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
     
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
