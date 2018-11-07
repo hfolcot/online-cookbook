ca7829d029891915afbc8682ed525b9dcfa4fe33
Heather Olcot <hfolcot@gmail.com>
2018-11-07 13:05:57 +0000

    connected app to database

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
-import os
+import os, pymongo
 
 from flask import Flask, render_template, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "online_cookbook"
+app.config["MONGO_URI"] = "mongodb://turnpike:n0tt00late@ds253203.mlab.com:53203/online_cookbook"
+
+mongo = PyMongo(app)
+
+
 
 @app.route("/")
 def index():
@@ -11,6 +19,16 @@ def index():
 @app.route("/add_recipe")
 def add_recipe():
     return render_template("add_recipe.html")
+
+@app.route("/results")
+def results():
+    return render_template("results.html", recipes=mongo.db.recipes.find())
+    
+@app.route("/recipe")
+def recipePage():
+    return render_template("recipe.html")
+    
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
