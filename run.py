72055e6a85667cc66191476ec7ca5dd8dd91f492
Heather Olcot <hfolcot@gmail.com>
2018-12-09 18:59:20 +0000

    added error handling for 500 error

--- run.py
+++ run.py
@@ -432,12 +432,16 @@ def delete_category():
     
     
 """
-404
+Error handling
 """
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+    
+@app.errorhandler(500)
+def something_wrong(error):
+    return render_template('500.html'), 500
 
 """
 Run the app
